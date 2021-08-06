5708d6ae1055b9a91e02b5f599055aa35cbde723
lucasw2025 <71618910+lucasw2025@users.noreply.github.com>
2021-08-05 20:49:55 -0500

    Update file2.lua

--- file2.lua
+++ file2.lua
@@ -15,3 +15,17 @@ print(baseTbl,metatable,func_example) -- testing what setmetatable returns
 -------------------------
 
 -- guessing for luau, most globals like CFrame and UDim2 are just metatables, as they have the __add to add, __mul for multiplying cframes, etc.
+
+-------------------------
+
+local metatableDescriptions = { -- description of these OOP objects
+	__index = "a table that's entries get applied to the table, if __index is a function, it is called and the item returned is used as the table",
+	__newindex = [[this function is called when something happens to the table, like tbl[key] = value, passing on tbl, key, and value to this function
+	rawset(tbl,key,value) skips this
+	]],
+	__call = "when this table is called as if it was a function, the function assigned to __call is executed",
+	__metatable = "if getmetatable(tbl) is called, and __metatable exists, it returns __metatable's value instead of the real metatable",
+	__tostring = "function called when tostring(metatable) is called and returns what __tostring's function returns",
+	__len = "returns what happens when attempting to use call operator ('#') on this metatable",
+	
+}
